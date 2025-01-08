5789398cea148dbd5cbf539b22c77cb10727ab4a
asma <abs@iyasoft.corp>
2025-01-08 10:09:57 +0100

    add logger and Serilog package

---
+++ Dtos/Helpers/GlobalExceptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+namespace WeatherApp.Dtos.Helpers
+{
+    public class GlobalExceptionHandler :IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext , Exception exception,CancellationToken cancellationToken)
+        {
+            var problemDetails = new ProblemDetails()
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "server error! Handled by global exception handler"
+            };
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails,cancellationToken);
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using Serilog;
 using WeatherApp.Components;
+using WeatherApp.Dtos.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+Log.Logger=new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("Logs/mylogs.txt",rollingInterval:RollingInterval.Day).CreateLogger();
+builder.Host.UseSerilog();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 
